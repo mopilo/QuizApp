66c39a938dc9c08898098bd4480ec6681e813ed1
mopilo <mopahemma@gmail.com>
2018-07-31 10:13:20 +0100

    style

--- lib/UI/answer_button.dart
+++ lib/UI/answer_button.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
 class AnswerButton extends StatelessWidget{
 
-  bool answer;
-  VoidCallback _onTap;
-  AnswerButton(this.answer, this._onTap);
+  final bool _answer;
+  final VoidCallback _onTap;
+  AnswerButton(this._answer, this._onTap);
 
   @override
   Widget build(BuildContext context){
     return new Expanded(
               child: new Material( //true button
-                color: answer == true ? Colors.blueAccent : Colors.redAccent,
+                color: _answer == true ? Colors.blueAccent : Colors.redAccent,
                 child: new InkWell(
                   onTap: ()=> _onTap(),
                   child: new Center(
                     child: new Container(
-                      child: new Text( answer == true ? "True" : "False"),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 4.0
+                        )
+                      ),
+                      padding: EdgeInsets.all(20.0),
+                      child: new Text( _answer == true ? "True" : "False",
+                        style: new TextStyle(
+                          color: Colors.white, fontSize: 40.0, fontWeight: FontWeight.bold, fontStyle: FontStyle.italic
+                        ),
+                      ),
                     ),
                   ),
                 ),
