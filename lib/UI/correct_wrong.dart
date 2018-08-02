8bd0a112b186da9da57b405e6a0ffaec9a988308
mopilo <mopahemma@gmail.com>
2018-08-02 14:27:58 +0100

    class correct

---
+++ lib/UI/correct_wrong.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CorrectWrongOverlay extends StatefulWidget{
+  @override
+  State createState()=> new CorrectWrongOverlayState();
+}
+
+class CorrectWrongOverlayState extends State<CorrectWrongOverlay>{
+   @override
+   Widget build(BuildContext context){
+     
+   }
+}
